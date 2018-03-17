2701fb3910b7069c23d3652cf3897e9a0a662e85
raksheen <raksheenayaz@gmail.com>
2018-03-17 17:43:31 -0400

    added screenshot

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # alarmclock
+
+Alarm clock created using JavaScript, HTML and CSS
+
+![alt text](Screenshot.png "alarm clock")
---
+++ clock.js
@@ -0,0 +1,153 @@
+
+var alarm = document.getElementById('alarm_clock');
+
+// display current time by the second
+var currentTime = setInterval(function(){
+	var date = new Date();
+	
+	var hours = (12 - (date.getHours()));
+	
+	var minutes = date.getMinutes();
+	
+	var seconds = date.getSeconds();
+	
+	var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
+
+
+	//military time to standard time
+
+	if (hours < 0) {
+		hours = hours * -1;
+	} else if (hours == 00) {
+		hours = 12;
+	} else {
+		hours = hours;
+	}
+
+	
+	alarm.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + " " + ampm;
+	
+},1000);
+
+function addZero(time) {
+
+		return (time < 10) ? "0" + time : time;
+	
+}
+
+function hoursMenu(){
+
+	var select = document.getElementById('alarmhrs');
+	var hrs = 12
+
+	for (i=1; i <= hrs; i++) {
+		select.options[select.options.length] = new Option( i < 10 ? "0" + i : i, i);
+		
+	}
+}
+hoursMenu();
+
+function minMenu(){
+
+	var select = document.getElementById('alarmmins');
+	var min = 59;
+
+	for (i=0; i <= min; i++) {
+		select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+	}
+}
+minMenu();
+
+function secMenu(){
+
+	var select = document.getElementById('alarmsecs');
+	var sec = 59;
+
+	for (i=0; i <= sec; i++) {
+		select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+	}
+}
+secMenu();
+
+
+function set() {
+
+	var hr = document.getElementById('alarmhrs');
+	
+	var min = document.getElementById('alarmmins');
+	
+	var sec = document.getElementById('alarmsecs');
+	
+	var ap = document.getElementById('ampm');
+    
+
+    var selectedHour = hr.options[hr.selectedIndex].value;
+    var selectedMin = min.options[min.selectedIndex].value;
+    var selectedSec = sec.options[sec.selectedIndex].value;
+    var selectedAMPM = ap.options[ap.selectedIndex].value;
+
+    var alarmTime = addZero(selectedHour) + ":" + addZero(selectedMin) + ":" + addZero(selectedSec) + selectedAMPM;
+    console.log('alarmTime:' + alarmTime);
+
+    document.getElementById('alarmhrs').disabled = true;
+	document.getElementById('alarmmins').disabled = true;
+	document.getElementById('alarmsecs').disabled = true;
+	document.getElementById('ampm').disabled = true;
+
+
+	var sound = new Audio("https://www.freespecialeffects.co.uk/soundfx/clocks/clock_chime.wav");
+		sound.loop = true;
+
+	var alarm = document.getElementById('clock');
+
+
+
+setInterval(function(){
+
+	var date = new Date();
+	
+	var hours = (12 - (date.getHours()));
+	// var hours = date.getHours();
+	
+	var minutes = date.getMinutes();
+	
+	var seconds = date.getSeconds();
+	
+	var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
+
+
+	//convert military time to standard time
+
+	if (hours < 0) {
+		hours = hours * (-1);
+	} else if (hours == 00) {
+		hours = 12;
+	} else {
+		hours = hours;
+	}
+	
+	var currentTime = alarm.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
+	
+
+	if (alarmTime == currentTime) {
+		sound.play();
+		}
+
+},1000);
+
+
+	// console.log('currentTime:' + currentTime);	
+
+}
+
+
+function Clear() {
+
+	document.getElementById('alarmhrs').disabled = false;
+	document.getElementById('alarmmins').disabled = false;
+	document.getElementById('alarmsecs').disabled = false;
+	document.getElementById('ampm').disabled = false;
+	sound.pause();
+}
+
+
